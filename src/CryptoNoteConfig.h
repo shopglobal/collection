fea51efe036ba882141da1ba4a01b3923e26fc15
Mark Allen Evans Jr <president@worldvaporexpo.com>
2017-12-01 02:59:22 -0500

    synchronized seed nodes - finalized prerequisites

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"your_seed_ip1.com:8080",
-  //"your_seed_ip2.com:8080",
+  "159.203.174.173:21000",
+  "174.138.38.137:21007",
 };
 
 struct CheckpointData {
