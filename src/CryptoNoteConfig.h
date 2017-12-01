6b3f9072e38ddd58793df10a3c6c74e3890f8009
Mark Allen Evans Jr <president@worldvaporexpo.com>
2017-12-01 06:02:00 -0500

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,6 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "volley";
-Insert this line into your coin configuration file as is: 
 const char GENESIS_COINBASE_TX_HEX[] = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121013b8803b3a40ad533c4d3fd5abcabaee56a3d59fdce3ae65afefc6a44a7ca87d3";
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
